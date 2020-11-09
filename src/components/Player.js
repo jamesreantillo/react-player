3234ccbe0440780a3a11fb341e9d0227a2320f65
james reantillo <james.reantillo@gmail.com>
2020-11-09 00:27:29 -0800

    install sass version 4.14.1

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Player = () => {
-  return ( 
-    <div className="player">
-      <div className="time-controlled">
+  return (
+    <div className='player'>
+      <div className='time-controlled'>
         <p>Start Time</p>
-        <input type="range"/>
+        <input type='range' />
         <p>End Time</p>
       </div>
-      <div className="play-control">
-
+      <div className='play-control'>
+        <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
+        <FontAwesomeIcon className='play' size='2x' icon={faPlay} />
+        <FontAwesomeIcon
+          className='skip-forward'
+          size='2x'
+          icon={faAngleRight}
+        />
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Player;
